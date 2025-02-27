a5edd74dd62e1e98d053d83afab02fece0ed9a42
Oince <39307905+Oince@users.noreply.github.com>
2025-02-27 19:02:26 +0900

    refactor(mentor): msg -> message로 변경

--- src/main/java/com/sprint/mission/discodeit/exception/FileIOException.java
+++ src/main/java/com/sprint/mission/discodeit/exception/FileIOException.java
@@ -3,7 +3,8 @@ package com.sprint.mission.discodeit.exception;
 import org.springframework.http.HttpStatus;
 
 public class FileIOException extends ExpectedException {
-    public FileIOException(String msg) {
-        super(HttpStatus.INTERNAL_SERVER_ERROR, msg);
-    }
+
+  public FileIOException(String message) {
+    super(HttpStatus.INTERNAL_SERVER_ERROR, message);
+  }
 }
