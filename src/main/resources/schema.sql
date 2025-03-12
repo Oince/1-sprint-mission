b2cd222ee9714fb4c1437c7e7a1385afac929ab5
Oince <39307905+Oince@users.noreply.github.com>
2025-03-12 20:34:21 +0900

    schema.sql 파일 추가

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,60 @@
+create table binary_contents(
+	id uuid primary key,
+	created_at timestamptz not null,
+	file_name varchar(255) not null,
+	size bigint not null,
+	content_type varchar(100) not null
+);
+
+create table users(
+	id uuid primary key,
+	created_at timestamptz not null,
+	updated_at timestamptz,
+	username varchar(50) not null unique,
+	email varchar(100) not null unique,
+	password varchar(60) not null,
+	profile_id uuid references binary_contents(id) on delete set null
+);
+
+create table user_statuses(
+	id uuid primary key,
+	created_at timestamptz not null,
+	updated_at timestamptz,
+	user_id uuid not null unique references users(id) on delete cascade,
+	last_active_at timestamptz not null
+);
+
+create type channel_type as enum('PRIVATE', 'PUBLIC');
+
+create table channels(
+	id uuid primary key,
+	created_at timestamptz not null,
+	updated_at timestamptz,
+	name varchar(100),
+	description varchar(500),
+	type channel_type not null
+);
+
+create table read_statuses(
+	id uuid primary key,
+	created_at timestamptz not null,
+	updated_at timestamptz,
+	user_id uuid references users(id) on delete cascade,
+	channel_id uuid references channels(id) on delete cascade,
+	last_read_at timestamptz not null,
+	constraint read_statuses_user_id_channel_id_key UNIQUE (user_id, channel_id)
+);
+
+create table messages(
+	id uuid primary key,
+	created_at timestamptz not null,
+	updated_at timestamptz,
+	content text,
+	channel_id uuid not null references channels(id) on delete cascade,
+	author_id uuid references users(id) on delete set null
+);
+
+create table message_attachments(
+	message_id uuid references messages(id) on delete cascade,
+	attachment_id uuid references binary_contents(id) on delete cascade
+);
