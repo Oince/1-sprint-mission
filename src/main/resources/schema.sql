f2bd726e40075f30a216c97cef473ae67cf2a56f
Oince <39307905+Oince@users.noreply.github.com>
2025-04-01 17:12:06 +0900

    refactor: 변경 사항 적용

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,6 +1,6 @@
 create table binary_contents(
 	id uuid primary key,
-	created_at timestamptz not null,
+	created_at timestamp not null,
 	file_name varchar(255) not null,
 	size bigint not null,
 	content_type varchar(100) not null
@@ -8,8 +8,8 @@ create table binary_contents(
 
 create table users(
 	id uuid primary key,
-	created_at timestamptz not null,
-	updated_at timestamptz,
+	created_at timestamp not null,
+	updated_at timestamp,
 	username varchar(50) not null unique,
 	email varchar(100) not null unique,
 	password varchar(60) not null,
@@ -18,18 +18,18 @@ create table users(
 
 create table user_statuses(
 	id uuid primary key,
-	created_at timestamptz not null,
-	updated_at timestamptz,
+	created_at timestamp not null,
+	updated_at timestamp,
 	user_id uuid not null unique references users(id) on delete cascade,
-	last_active_at timestamptz not null
+	last_active_at timestamp not null
 );
 
 create type channel_type as enum('PRIVATE', 'PUBLIC');
 
 create table channels(
 	id uuid primary key,
-	created_at timestamptz not null,
-	updated_at timestamptz,
+	created_at timestamp not null,
+	updated_at timestamp,
 	name varchar(100),
 	description varchar(500),
 	type channel_type not null
@@ -37,18 +37,18 @@ create table channels(
 
 create table read_statuses(
 	id uuid primary key,
-	created_at timestamptz not null,
-	updated_at timestamptz,
+	created_at timestamp not null,
+	updated_at timestamp,
 	user_id uuid references users(id) on delete cascade,
 	channel_id uuid references channels(id) on delete cascade,
-	last_read_at timestamptz not null,
+	last_read_at timestamp not null,
 	constraint read_statuses_user_id_channel_id_key UNIQUE (user_id, channel_id)
 );
 
 create table messages(
 	id uuid primary key,
-	created_at timestamptz not null,
-	updated_at timestamptz,
+	created_at timestamp not null,
+	updated_at timestamp,
 	content text,
 	channel_id uuid not null references channels(id) on delete cascade,
 	author_id uuid references users(id) on delete set null
